231950df84f4770edefc6ab703a8a86216f0157e
zhenjie zhu <tech@intellij.io>
2025-07-30 17:08:35 +0800

    🌟 Simplify FRP versioning and enhance build automation
    Removed hardcoded FRP version from Docker Compose files, switching to the latest tag for consistency. Enhanced `build.sh` to dynamically detect and build all FRP versions, pushing the latest version as the default. Updated metadata in `Dockerfile` and improved formatting for better readability.

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,22 @@ FROM registry.cn-shanghai.aliyuncs.com/iproute/ubuntu:22.04
 
 ARG FRP_VERSION=0.61.1
 
-MAINTAINER "devops@kubectl.net"
+LABEL org.opencontainers.image.authors="tech@intellij.io"
 
-LABEL email="devops@kubectl.net" \
+LABEL email="tech@intellij.io" \
       author="zhenjie zhu"
 
 # random_file
 RUN dd if=/dev/urandom of=/random_file bs=1K count=1
 
-RUN sed -i 's@//.*archive.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list &&\
-      sed -i 's@//security.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list &&\
-      sed -i 's@//ports.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list &&\
-      apt-get update &&\
-      export DEBIAN_FRONTEND=noninteractive &&\
-      apt-get install -y iputils-ping ca-certificates vim curl &&\
-      update-ca-certificates &&\
-         apt-get clean && rm -rf /var/lib/apt/lists/*
+RUN sed -i 's@//.*archive.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list && \
+  sed -i 's@//security.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list && \
+  sed -i 's@//ports.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list && \
+  apt-get update && \
+  export DEBIAN_FRONTEND=noninteractive && \
+  apt-get install -y iputils-ping ca-certificates vim curl && \
+  update-ca-certificates && \
+  apt-get clean && rm -rf /var/lib/apt/lists/*
 
 ADD build_files/frp_${FRP_VERSION}_linux_amd64.tar.gz /opt/
 
--- build.sh
+++ build.sh
@@ -2,16 +2,43 @@
 # shellcheck disable=SC2164  disable=SC1090
 SHELL_FOLDER=$(cd "$(dirname "$0")" && pwd)
 cd "$SHELL_FOLDER"
+source <(curl -sSL "https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh")
 
-source <(curl -sSL "https://dev.kubectl.net/func/ostype.sh")
+source <(curl -sSL "$ROOT_URI/func/log.sh")
+source <(curl -sSL "$ROOT_URI/func/ostype.sh")
+
 if is_windows; then
   export MSYS_NO_PATHCONV=1
 fi
 
-FRP_VERSION="0.63.0"
-bash <(curl -sSL https://dev.kubectl.net/docker/build.sh) \
-  -i "registry.cn-shanghai.aliyuncs.com/iproute/frp" \
-  -a "FRP_VERSION=$FRP_VERSION" \
-  -v "$FRP_VERSION" \
-  -r "false" \
-  -p "true"
+versions=()
+mapfile -t files < <(ls build_files)
+# 获取所有 frp 版本号
+for file in "${files[@]}"; do
+  if [[ $file =~ frp_([0-9]+\.[0-9]+\.[0-9]+)_linux_amd64\.tar\.gz ]]; then
+    versions+=("${BASH_REMATCH[1]}")
+  fi
+done
+
+LAST_VERSION="${versions[-1]}"
+
+for version in "${versions[@]}"; do
+  log_info "build" "version is $version"
+  FRP_VERSION="$version"
+  bash <(curl -sSL "$ROOT_URI/docker/build.sh") \
+    -i "registry.cn-shanghai.aliyuncs.com/iproute/frp" \
+    -a "FRP_VERSION=$FRP_VERSION" \
+    -v "$FRP_VERSION" \
+    -r "false" \
+    -p "true"
+
+  if [[ "$FRP_VERSION" == "$LAST_VERSION" ]]; then
+    log_info "build" "last version is $FRP_VERSION, push to registry"
+    bash <(curl -sSL "$ROOT_URI/docker/build.sh") \
+      -i "registry.cn-shanghai.aliyuncs.com/iproute/frp" \
+      -a "FRP_VERSION=$FRP_VERSION" \
+      -v "latest" \
+      -r "false" \
+      -p "true"
+  fi
+done
